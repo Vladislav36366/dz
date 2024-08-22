80fd2a2bf0acacef335eb57641c5c3609d61b385
Vladislav36366 <mihailo04@mail.ru>
2024-08-22 13:56:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st program
+print(9 ** 0.5 *5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 +2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#4th program
+name= "123.456"
+print(name[4])
+print(int((float(name)*10)) % 10)
+
+
